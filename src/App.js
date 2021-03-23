f874bd4a6b26a5bd9c98ff969e5fde9db7e9f93c
zsc <zsc_worker@163.com>
2021-03-24 02:09:00 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import routes from '@/routes';
+import BaseLayout from '@/layout/BaseLayout';
 import './App.css';
+import './init.scss';
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
         <Switch>
-          {
-            routes.map(route => {
-              const { Comp, path, exact } = route;
-              return (
-                <Route key={path} exact={exact} path={path}>
-                  <Comp />
-                </Route>
-              )
-            })
-          }
+          <BaseLayout>
+            {
+              routes.map(route => {
+                const { Comp, path, exact } = route;
+                return (
+                  <Route key={path} exact={exact} path={path}>
+                    <Comp />
+                  </Route>
+                )
+              })
+            }
+          </BaseLayout>
         </Switch>
       </HashRouter>
     </div>
